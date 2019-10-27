256449a31bf865e23501e65853e2ba7effd00ffe
khangnguyen <khang.nguyen@giosg.com>
2019-10-27 13:01:37 +0200

    Update axiosConfig.js

--- axiosConfig.js
+++ axiosConfig.js
@@ -5,14 +5,26 @@ const initiateAxiosConfig = () => {
   axios.defaults.baseURL = 'https://gateway.eu1.mindsphere.io';
   axios.defaults.headers.common['Authorization'] = 'example';
   axios.defaults.headers.post['Content-Type'] = 'application/json';
+  // Interceptors to check the request before calling
+  axios.interceptors.request.use((request) => {
+    // If the request is to renew the token, then dont include Authorization in header
+    if (request.url === '/api/technicaltokenmanager/v3/oauth/token'){
+      request.headers.common['Authorization'] = '';
+    }
+    return request
+  })
+
+  // Interceptors to check the response
   axios.interceptors.response.use(
     (response) => {
       return response
     },
     (error) => {
       const originalRequest = error.config;
+      // If the response is unauthorized (401) or access denied (403), then renew the token and include new token in header, then execute the failed request again.
       if ((error.response.status === 401 && !originalRequest._retry) || (error.response.status === 403 && !originalRequest._retry)) {
         originalRequest._retry = true;
+        // Process of renew the token
         const clientId = process.env.MDSP_KEY_STORE_CLIENT_ID;
         const clientSecret = process.env.MDSP_KEY_STORE_CLIENT_SECRET;
         const encodedString = Buffer.from(`${clientId}:${clientSecret}`).toString('base64')
@@ -33,7 +45,7 @@ const initiateAxiosConfig = () => {
               // Change Authorization header
               axios.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.access_token;
               originalRequest.headers['Authorization'] = 'Bearer ' + res.data.access_token;
-              // return originalRequest object with Axios.
+              // return originalRequest object with Axios. It means that execute the failed request
               return axios.request(originalRequest);
             })
         } catch (err) {
