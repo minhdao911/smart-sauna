defa167aa185abe2b73187773e70cbfb05702364
khangnguyen <khang.nguyen@giosg.com>
2019-10-28 21:05:57 +0200

    return single object with room name

--- controllers.js
+++ controllers.js
@@ -4,7 +4,10 @@ const getRoomData = async (req, res, next) => {
   const room = req.query.room;
   try {
     const saunaSeriesDataRes = await axios.get(`/api/iottimeseries/v3/timeseries/1904951b1c6740cdbe77bb0a9d5c86ee/${room}`)
-    res.status(200).json(saunaSeriesDataRes.data)
+    res.status(200).json({
+      ...saunaSeriesDataRes.data[0],
+      room,
+    })
   } catch (err) {
     console.log(err)
     // console.log(err.response.status);
