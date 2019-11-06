01fac5a3e3f64bf7109517427e24933a9d5d7da1
Minh Dao <daominh91197@gmail.com>
2019-11-07 00:18:34 +0200

    fix file name error

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.scss';
 
-import { NavLink  } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import logo from '../../assets/images/logo-small.png';
 
