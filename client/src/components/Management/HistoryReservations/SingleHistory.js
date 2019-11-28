46795e68a3b4e006ced1d5955e99b92733654282
khangnguyen <khang.nguyen@giosg.com>
2019-11-28 17:32:04 +0200

    Styling

--- client/src/components/Management/HistoryReservations/SingleHistory.js
+++ client/src/components/Management/HistoryReservations/SingleHistory.js
@@ -1,13 +1,21 @@
 import React from 'react';
-
+import { Row, Col, Icon } from 'antd';
 
 const SingleHistory = ({history}) => {
   return (
-    <>
-      <div>
-        {history.room.name} | {history.timeslot} | {history.date}      {history.room.temperature}   {history.room.humidity}
-      </div>
-    </>
+    <Row className="history-container">
+      <Col span={18}>
+        {history.room.name}&nbsp;&nbsp;&nbsp;
+        <Icon type="clock-circle" /> {history.timeslot}&nbsp;&nbsp;&nbsp;
+        <Icon type="calendar" /> {history.date}
+      </Col>
+      <Col span={3}>
+      {history.room.temperature}
+      </Col>
+      <Col span={3}>
+        {history.room.humidity}
+      </Col>
+    </Row>
   )
 };
 
