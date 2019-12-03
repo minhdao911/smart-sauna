93eec00651a3c23e587e3dc102d1036640c732e2
khangnguyen <khang.nguyen@giosg.com>
2019-12-03 14:19:05 +0200

    only use dotenv when not production

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 const routes = require('./routes');
 const initiateAxiosConfig = require('./axiosConfig');
-require('dotenv').config()
+
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+    require('dotenv').config()
+}
 
 initiateAxiosConfig();
 app.use(bodyParser.json());
