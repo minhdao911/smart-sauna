6de84c1a2a1083ffdd158cf59842ea6e426de393
Minh Dao <daominh91197@gmail.com>
2019-12-03 15:27:37 +0200

    load server base on node env

--- client/src/apis/index.js
+++ client/src/apis/index.js
@@ -1,4 +1,8 @@
-const baseUrl = 'https://smart-sauna-server.herokuapp.com'
+let baseUrl = process.env.REACT_APP_DEV_SERVER;
+
+if(process.env.NODE_ENV === 'production'){
+    baseUrl = process.env.REACT_APP_PROD_SERVER;
+}
 
 const getRoomData = (room) => `${baseUrl}/get_room_data?room=${room}`;
 const getTotalRooms = () => `${baseUrl}/get_total_rooms`;
