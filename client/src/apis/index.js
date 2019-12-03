b19f7b4e990ccf7f17fe4956ed5a3b3b82d1556e
khangnguyen <khang.nguyen@giosg.com>
2019-12-03 14:26:09 +0200

    use server production base url

--- client/src/apis/index.js
+++ client/src/apis/index.js
@@ -1,5 +1,7 @@
-const getRoomData = (room) => `/get_room_data?room=${room}`;
-const getTotalRooms = () => '/get_total_rooms';
+const baseUrl = 'https://smart-sauna-server.herokuapp.com'
+
+const getRoomData = (room) => `${baseUrl}/get_room_data?room=${room}`;
+const getTotalRooms = () => `${baseUrl}/get_total_rooms`;
 
 export default {
     getRoomData,
